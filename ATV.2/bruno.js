c70a165fe42b22169cd6de367f4f8ce8d31c2558
brunnontj <bruno.r.brito@aluno.senai.br>
2024-01-29 17:47:25 -0300

    atividade - 5

--- ATV.2/bruno.js
+++ ATV.2/bruno.js
@@ -1,4 +1,11 @@
 let Nub1 = prompt("digite um numero")
 if(Nub1>0) {console.log("positivo:"+Nub1);}
 else if(Nub1<0) {console.log("negativo:"+Nub1);}
-else {console.log("nulo:"+Nub1);}
+let num = prompt("digite um numero");
+let contador =1;
+
+while(contador<=10){ 
+let result = num*contador;
+console.log(num+"x"+contador+"="+result);
+contador=contador+1;
+}
