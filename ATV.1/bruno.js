861e95a25dc5c2ef3e9e537c4e453d8370a34223
brunnontj <bruno.r.brito@aluno.senai.br>
2024-01-29 16:55:45 -0300

    atividade-1

---
+++ ATV.1/bruno.js
@@ -0,0 +1,13 @@
+let siuu = 10
+let siuu1 = prompt ("digite um numero");
+const soma =  Number(siuu) + Number(siuu1)
+const sub = siuu - siuu1
+const div = siuu / siuu1
+const mult = siuu * siuu1
+const porc = siuu % siuu1
+
+console.log (soma)
+console.log (sub)
+console.log (div)
+console.log (mult)
+console.log (porc)
---
+++ ATV.2/bruno.js
@@ -0,0 +1,4 @@
+let Nub1 = prompt("digite um numero")
+if(Nub1>0) {console.log("positivo:"+Nub1);}
+else if(Nub1<0) {console.log("negativo:"+Nub1);}
+else {console.log("nulo:"+Nub1);}
