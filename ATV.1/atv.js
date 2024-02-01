ac914f60c78275293a8cd8d9a5d6fd005656876f
brunnontj <bruno.r.brito@aluno.senai.br>
2024-02-01 14:36:29 -0300

    siuuuuuuuuu

---
+++ ATV.1/atv.js
@@ -0,0 +1,28 @@
+let nota = prompt("Diga a sua nota meu nobre.");
+let conceito;
+
+switch (true){
+   case nota >= 0 && nota <= 59;
+   conceito = "F";
+   break;
+    case nota >= 60 && nota <= 69;
+   conceito = "D";
+   break;
+    case nota >= 70 && nota <= 79;
+   conceito = "C";
+   break;
+    case nota >= 80 && nota <= 89;
+   conceito = "B";
+   break;
+    case nota >= 90 && nota <= 100;
+   conceito = "A";
+   break;
+   default:
+        console.log("Nota inválida");
+        
+}
+
+if (conceito) {
+        console.log('conceito: $(conceito)');
+
+}
