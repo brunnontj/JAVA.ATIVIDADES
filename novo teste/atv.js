167abbe32e1542ad67e0d2cbe694d141d4d7f02d
brunnontj <bruno.r.brito@aluno.senai.br>
2024-02-05 17:03:48 -0300

    atividade pronta

--- novo teste/atv.js
+++ novo teste/atv.js
@@ -1,6 +1,12 @@
 function login() {
-    const username = document.getElementById("e-mail").value;
-    const pa = document.getElementById("senha").value;
+    const emaill = document.getElementById("e-mail").value;
+    const senha = document.getElementById("senha").value;
+
+
+if (email === "aluno2" && senha === "senai") {
+  alert("login bem-sucedido!");
+} else {
+    alert("crendeciais onválidas.tente novamente.");
 }
 
-if (email) === ""
+}
