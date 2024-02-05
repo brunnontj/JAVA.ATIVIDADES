b7b6896b575451ba9c31ca318699eda1169635f2
brunnontj <bruno.r.brito@aluno.senai.br>
2024-02-05 18:00:47 -0300

    atvvvvv

---
+++ Questão bonus/atv.js
@@ -0,0 +1,12 @@
+let aluno = prompt("Calcular o Índice de massa corporal");
+
+ if (aluno == "pedro" ){
+    console.log("seu IMC é 20,2020, peso adequado")
+}
+else if (aluno == "maria" ){
+    console.log("seu IMC é 21,6049, peso adequado")
+}
+else  {
+   console.log("sem matricula aqui!")
+
+}
