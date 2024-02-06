97c88345384dadcb15b6e4356af1d80425f0fbf1
brunnontj <bruno.r.brito@aluno.senai.br>
2024-02-06 17:44:43 -0300

    Na luta!!!

--- atv login/atv.js
+++ atv login/atv.js
@@ -1,4 +1,4 @@
-function login() {
+function entrar() {
   //Obter os valores do campo de entrada
     const email = document.getElementById("e-mail").value;
     const senha = document.getElementById("senha").value;
